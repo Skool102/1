491294c5c11c59fa0edb054dc7b29d66f050e441
Skool102 <114787941+Skool102@users.noreply.github.com>
2022-10-01 08:45:20 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace Buoi02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCham = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtThreads = new System.Windows.Forms.TextBox();
+            this.btnNhanh = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lbKetqua = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnCham
+            // 
+            this.btnCham.Location = new System.Drawing.Point(251, 5);
+            this.btnCham.Name = "btnCham";
+            this.btnCham.Size = new System.Drawing.Size(110, 26);
+            this.btnCham.TabIndex = 0;
+            this.btnCham.Text = "Ưu tiên thấp";
+            this.btnCham.UseVisualStyleBackColor = true;
+            this.btnCham.Click += new System.EventHandler(this.btnCham_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nhập số thread cần chạy";
+            // 
+            // txtThreads
+            // 
+            this.txtThreads.Location = new System.Drawing.Point(145, 9);
+            this.txtThreads.Name = "txtThreads";
+            this.txtThreads.Size = new System.Drawing.Size(100, 20);
+            this.txtThreads.TabIndex = 2;
+            // 
+            // btnNhanh
+            // 
+            this.btnNhanh.Location = new System.Drawing.Point(367, 5);
+            this.btnNhanh.Name = "btnNhanh";
+            this.btnNhanh.Size = new System.Drawing.Size(74, 26);
+            this.btnNhanh.TabIndex = 3;
+            this.btnNhanh.Text = "Ưu tiên cao";
+            this.btnNhanh.UseVisualStyleBackColor = true;
+            this.btnNhanh.Click += new System.EventHandler(this.btnNhanh_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(447, 5);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(109, 26);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Tính tích 2 ma trận";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lbKetqua
+            // 
+            this.lbKetqua.FormattingEnabled = true;
+            this.lbKetqua.Location = new System.Drawing.Point(12, 35);
+            this.lbKetqua.Name = "lbKetqua";
+            this.lbKetqua.Size = new System.Drawing.Size(544, 420);
+            this.lbKetqua.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(568, 450);
+            this.Controls.Add(this.lbKetqua);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnNhanh);
+            this.Controls.Add(this.txtThreads);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCham);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCham;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtThreads;
+        private System.Windows.Forms.Button btnNhanh;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox lbKetqua;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Buoi02
+
+{
+    public partial class Form1 : Form
+    {
+        //Định nghĩa class đối tưởng chứa các tham số cần truyền cho thread con
+        class Params
+        {
+            //Đối tượng quản lý thread tương ứng
+            public Thread t;
+            //Hàng bắt đầu tính
+            public int sr;
+            //Hàng kết thúc tính + 1
+            public int er;
+            //Chỉ số thread trong danh sách quản lý
+            public int id;
+            public Params(Thread t, int s, int e, int i)
+            {
+                this.t = t;
+                sr = s;
+                er = e;
+                id = i;
+            }
+        };
+        //Định nghĩa 3 biến ma trận
+        double[,] A;
+        double[,] B;
+        double[,] C;
+        //Định nghĩa biến chứa số hàng/cột của ma trận
+        int N;
+        //Định nghĩa danh sách trạng thái thi hành của các thread con
+        int[] stateLst = new int[20];
+        //Định nghĩa danh sách thời gian thi hành của các thread con
+        System.TimeSpan[] dateLst = new System.TimeSpan[20];
+        //Định nghĩa biến miêu tả quyền ưu tên của chương trình
+        ProcessPriorityClass myPrio = ProcessPriorityClass.Normal;
+        Process MyProc;
+        //Định nghĩa danh sách các quyền ưu tiên cho các thread
+        ThreadPriority[] tPrio =
+        {
+            ThreadPriority.Lowest, ThreadPriority.BelowNormal, ThreadPriority.Normal,
+            ThreadPriority.AboveNormal, ThreadPriority.Highest
+        };
+        //Định nghĩa hàm các hàng ma trận tích theo yêu cầu trong tham số
+        void TinhTich(object obj)
+        {
+            DateTime t1 = DateTime.Now;
+            Params p = (Params)obj;
+            int h, c, k;
+            for (h = p.sr; h < p.er; h++)
+                for (c = 0; c < N; c++)
+                {
+                    double s = 0;
+                    for (k = 0; k < N; k++)
+                        s = s + A[h, k] * B[k, c];
+                    C[h, c] = s;
+                }
+            //Ghi nhận đã hoàn thành nhiệm vụ
+            stateLst[p.id] = 1;
+            //Ghi nhận thời gian tính
+            dateLst[p.id] = DateTime.Now.Subtract(t1);
+        }
+
+        public Form1()
+        {
+
+            InitializeComponent();
+            //Thêm vào các lệnh khởi tạo sau đây
+            lbKetqua.Items.Clear();
+            //Khởi tạo các ma trận A, B, C
+            N = 1000;
+            A = new double[N, N];
+            B = new double[N, N];
+            C = new double[N, N];
+            int h, c;
+            for (h = 0; h < N; h++)
+                for (c = 0; c < N; c++)
+                    A[h, c] = B[h, c] = c;
+        }
+
+
+        private void btnCham_Click(object sender, EventArgs e)
+        {
+            //Thiết lập chế độ quyền ưu tiên realtime cho chương trình
+            myPrio = ProcessPriorityClass.BelowNormal;
+        }
+
+        private void btnNhanh_Click(object sender, EventArgs e)
+        {
+            //Thiết lập chế độ quyền ưu tiên realtime cho chương trình
+            myPrio = ProcessPriorityClass.RealTime;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //Xác định đối tượng quản lý process hiện hành
+            MyProc = Process.GetCurrentProcess();
+            //Thay đổi quyền ưu tiên theo yêu cầu người dùng
+            MyProc.PriorityClass = myPrio;
+            //Xác định số thread tham gia tính tích 2 ma trận
+            int cnt = Int32.Parse(txtThreads.Text);
+            int i;
+            //Ghi nhận thời điểm bắt đầu tính tích
+            DateTime t1 = DateTime.Now;
+            if (cnt == 1)
+                //Dùng thuật ngữ giải tuần tự
+                TinhTich(new Params(null, 0, N, 0));
+            else
+            {
+                //Dùng thuật giải song song gồm cnt-1 thread con và 1 thread chính có sẵn
+                Thread t;
+                //Lập tạo và chạy từng thread con
+                for (i = 0; i < cnt - 1; i++)
+                {
+                    //Ghi nhận thread i chưa chạy xong
+                    stateLst[i] = 0;
+                    //Tạo thread mới để chạy hàm TinhTich
+                    t = new Thread(new ParameterizedThreadStart(TinhTich));
+                    //Thiết lập quyền ưu tiên cho thread i
+                    t.Priority = tPrio[i % 5];
+                    //Hiển thị độ ưu tiên của thread i
+                    lbKetqua.Items.Add(String.Format("Thread {0:d} co do uu tien = {1:d}", i, t.Priority.ToString()));
+                    //Kích hoạt thread i chạy và truyền các tham số cần thiết cho nó
+                    t.Start(new Params(t, i * N / cnt, (i + 1) * N / cnt, i));
+                }
+                //Bản thân thread cha cũng tính N/cnt hàng cuối của ma trận tích
+                TinhTich(new Params(null, (cnt - 1) * N / cnt, N, cnt - 1));
+                //Chờ đợi các thread con hoàn thành
+                for (i = 0; i < cnt - 1; i++)
+                    //Cho
+                    while (stateLst[i] == 0) ;
+
+            }
+            //Ghi nhận thời điểm kết thúc tính tích
+            DateTime t2 = DateTime.Now;
+            System.TimeSpan diff;
+            //Hiển thị độ ưu tiên hiện hành của chương trình
+            lbKetqua.Items.Add("Ung dung da chay voi quyen" + myPrio.ToString());
+            //Hiển thị thời gian tính của từng thread con
+            for (i = 0; i < cnt - 1; i++)
+            {
+                diff = dateLst[i]
+;
+                lbKetqua.Items.Add(String.Format("Thread {0:d} chay ton {1:d2} phut {2:d2} giay {3:d3} ms", i, diff.Minutes, diff.Seconds, diff.Milliseconds));
+            }
+            diff = t2.Subtract(t1);
+            //Hiển thị thời gian tổng cộng để tính tích
+            lbKetqua.Items.Add(String.Format("{0:d} threads ==> Thoi gian chay la {1:d2} phut {2:d2} giay {3:d3} ms", cnt, diff.Minutes, diff.Seconds, diff.Milliseconds));
+        }
+    }
+}
